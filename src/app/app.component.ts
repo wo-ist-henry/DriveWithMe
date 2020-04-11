bf0e5725f41da9bdfecc6d6d5352c01cc7c66b11
Henry <bluemlein4@web.de>
2020-04-11 21:40:23 +0200

    MediaQuery for Adddialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,38 @@
 import {Component} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {AddCarpoolComponent} from './home/add-drive/add-carpool.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DriveWithMe';
+    title = 'DriveWithMe';
+    private isMobileResolution: boolean;
 
-  constructor(public dialog: MatDialog) {
-  }
+    constructor(public dialog: MatDialog) {
+        if (window.innerWidth < 768) {
+            this.isMobileResolution = true;
+        } else {
+            this.isMobileResolution = false;
+        }
+    }
 
-  addDrive(): void {
-    const dialogRef = this.dialog.open(AddCarpoolComponent, {
-      width: '35%',
-    });
+    addDrive(): void {
+        let dialogRef: MatDialogRef<AddCarpoolComponent, any>;
+        if (!this.isMobileResolution){
+             dialogRef = this.dialog.open(AddCarpoolComponent, {
+                width: '35%',
+            });
+        } else {
+            dialogRef = this.dialog.open(AddCarpoolComponent, {
+                width: '85%',
+            });
+        }
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
 }
