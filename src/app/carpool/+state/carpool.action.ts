37f1fcb37c60825cc7d54c86866c9973e608d48a
Henry <henrypaul.thoma@gmail.com>
2020-07-09 20:27:25 +0200

    Delete NgRx Files and install Docker

--- src/app/carpool/+state/carpool.action.ts
+++
@@ -1,6 +0,0 @@
-import {createAction, props} from '@ngrx/store';
-import {Carpool} from '../../models/carpool';
-
-export const loadCarpools = createAction(
-    '[Carpools] Loading Carpools',
-    props<{ carpools: Carpool[] }>());
--- src/app/carpool/carpool.module.ts
+++ src/app/carpool/carpool.module.ts
@@ -3,13 +3,8 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Routes, RouterModule} from '@angular/router';
 import {MatDialogModule} from '@angular/material/dialog';
-
 import {IonicModule} from '@ionic/angular';
-
 import {CarpoolPage} from './carpool.page';
-import {StoreModule} from '@ngrx/store';
-
-import * as fromCarpools from './+state/carpool.reducer';
 
 const routes: Routes = [
     {
@@ -25,7 +20,6 @@ const routes: Routes = [
         IonicModule,
         RouterModule.forChild(routes),
         MatDialogModule,
-        StoreModule.forFeature(fromCarpools.carpoolFeatureKey, fromCarpools.carpoolReducer)
     ],
     declarations: [CarpoolPage]
 })
